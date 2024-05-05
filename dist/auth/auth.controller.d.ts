5404223f296a655cd61985df17f3948a5f08e2b0
Hardik Upadhyay <hardik89990@gmail.com>
2024-05-05 14:46:56 +0530

    Delete

--- dist/auth/auth.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from "express";
-import { AuthService } from "./auth.service";
-export declare class AuthController {
-    private readonly userService;
-    constructor(userService: AuthService);
-    insertFirst(req: Request, res: Response): Promise<Response<any, Record<string, any>>>;
-    loginFirst(req: Request, res: Response): Promise<Response<any, Record<string, any>>>;
-    checkguard(req: any, res: Response): Promise<Response<any, Record<string, any>>>;
-}
